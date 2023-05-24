cf480027a201e8f575e4001a67496c08d8ba2ba1
LONOAL <lnogueirapan@danielcastelao.org>
2023-05-24 13:41:50 +0200

    first commit with UI updated

--- src/View.java
+++ src/View.java
@@ -2,7 +2,7 @@ import java.awt.*;
 
 public class View {
     void muestraVelocidad(String matricula, Integer v){
-        Dialog.vDialogo("El coche " + matricula + " tiene una velocidad de: " + v + "km/h");
+        //Dialog.vDialogo("El coche " + matricula + " tiene una velocidad de: " + v + "km/h");
 
     }
 }
